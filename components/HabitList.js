4d569a93b6f70e911d075a590f480e3bfa91dd04
juliansal <juliansal.slc@gmail.com>
2019-12-12 01:45:27 -0800

    uncomment list

--- components/HabitList.js
+++ components/HabitList.js
@@ -2,44 +2,39 @@ import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 import Habit from './Habit'
 
-// const GET_HABITS = gql`
-// 	query getHabits {
-// 		habits {
-// 			_id
-// 			name
-// 			events {
-// 				_id
-// 				date
-// 			}
-// 		}
-// 	}
-// `;
-
-// const GET_HABITS = gql`
-// 	query HelloQuery {
-// 		sayHello
-// 	}`
+const GET_HABITS = gql`
+	query getHabits {
+		habits {
+			_id
+			name
+			events {
+				_id
+				date
+			}
+		}
+	}
+`;
 
 const HabitList = () => {
-	// const { data, loading, error } = useQuery(GET_HABITS)
+	const { data, loading, error } = useQuery(GET_HABITS)
 
-	// if (loading) {
-	// 	console.log("Getting a loading screen " + loading)
-	// 	return (<section />)
-	// }
-	// if (error) {
-	// 	console.log("The error occurred in the HabitList" + error)
-	// 	console.log("The data is " + data)
-	// 	return (<section />)
-	// }
-	// const { habits } = data
+	if (loading) {
+		console.log("Getting a loading screen " + loading)
+		return (<section />)
+	}
+	if (error) {
+		console.log("The error occurred in the HabitList" + error)
+		console.log("The data is " + data)
+		return (<section />)
+	}
+	const { habits } = data
 	
 	return (
 		<section>
 			<h2>My Habits List</h2>
-			{/* { habits.map((habit, index) => (
+			{ habits.map((habit, index) => (
 				<Habit key={ habit._id } habit={ habit } index={ index }/>
-			)) } */}
+			)) }
 		</section>
 	)
 }
